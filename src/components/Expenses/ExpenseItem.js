74488ea8d7880601903f1a23146f08b3e4e34c79
rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>
2023-04-08 17:33:10 +0530

    practising useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,27 @@
-import React, {useState} from "react";
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // let title = props.title;
-
+  // function clickHandler() {}
   const [title, setTitle] = useState(props.title);
-
+  console.log('ExpenseItem evaluated by React');
+  
   const clickHandler = () => {
-    setTitle('Updated!!');
-    // console.log(title);
-  }
+    setTitle('Updated!');
+    console.log(title);
+  };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <div className="expense-item__description">
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,57 +1,92 @@
-import React from "react";
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  // return (
-  //   <Card className="expenses">
-  //     {props.items.map((item) => (
-  //       <ExpenseItem
-  //         title={item.title}
-  //         amount={item.amount}
-  //         date={item.date}
-  //       ></ExpenseItem>
-  //     ))}
-  //   </Card>
-  // );
-
-  const expenseComponent = props.items.map((item) => (
-  <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
-  ));
-
-  return <Card className="expenses">{expenseComponent}</Card>;
-
-  // return <div className="expenses">{expenseComponent}</div>;
-
-
-  // return (
-  //   <Card className="expenses">
-  //     <ExpenseItem
-  //       title={props.items[0].title}
-  //       amount={props.items[0].amount}
-  //       date={props.items[0].date}
-  //     ></ExpenseItem>
-
-  //     <ExpenseItem
-  //       title={props.items[1].title}
-  //       amount={props.items[1].amount}
-  //       date={props.items[1].date}
-  //     ></ExpenseItem>
-
-  //     <ExpenseItem
-  //       title={props.items[2].title}
-  //       amount={props.items[2].amount}
-  //       date={props.items[2].date}
-  //     ></ExpenseItem>
-
-  //     <ExpenseItem
-  //       title={props.items[3].title}
-  //       amount={props.items[3].amount}
-  //       date={props.items[3].date}
-  //     ></ExpenseItem>
-  //   </Card>
-  // );
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
 }
 
 export default Expenses;
+
+// import React from "react";
+// import ExpenseItem from "./ExpenseItem";
+// import Card from "../UI/Card";
+// import './Expenses.css';
+
+// const Expenses = (props) => {
+//   // return (
+//   //   <Card className="expenses">
+//   //     {props.items.map((item) => (
+//   //       <ExpenseItem
+//   //         title={item.title}
+//   //         amount={item.amount}
+//   //         date={item.date}
+//   //       ></ExpenseItem>
+//   //     ))}
+//   //   </Card>
+//   // );
+
+//   const expenseComponent = props.items.map((item) => (
+//   <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+//   ));
+
+//   return <Card className="expenses">{expenseComponent}</Card>;
+
+//   // return <div className="expenses">{expenseComponent}</div>;
+
+
+//   // return (
+//   //   <Card className="expenses">
+//   //     <ExpenseItem
+//   //       title={props.items[0].title}
+//   //       amount={props.items[0].amount}
+//   //       date={props.items[0].date}
+//   //     ></ExpenseItem>
+
+//   //     <ExpenseItem
+//   //       title={props.items[1].title}
+//   //       amount={props.items[1].amount}
+//   //       date={props.items[1].date}
+//   //     ></ExpenseItem>
+
+//   //     <ExpenseItem
+//   //       title={props.items[2].title}
+//   //       amount={props.items[2].amount}
+//   //       date={props.items[2].date}
+//   //     ></ExpenseItem>
+
+//   //     <ExpenseItem
+//   //       title={props.items[3].title}
+//   //       amount={props.items[3].amount}
+//   //       date={props.items[3].date}
+//   //     ></ExpenseItem>
+//   //   </Card>
+//   // );
+// }
+
+// export default Expenses;
