45a0458e2ef73d2b073f999e231236821f6649f0
rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>
2023-05-02 23:31:31 +0530

    more on conditional rendering

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpenseList = (props) => {
+  if (props.item.length === 0) {
+    return <h2 className="expenses-list__fallback">No expenses found.</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.item.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      ;
+    </ul>
+  );
+};
+
+export default ExpenseList;
